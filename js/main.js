e2123d664d69a94de296a8fa8aa3b9f9ff85504d
Ce <celiarosalescaballero@gmail.com>
2022-03-29 10:51:24 +0200

    add css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 /*En la parte inferior debe aparecer la cantidad de dinero que tiene la jugadora. Agrega el código
 necesario para que la usuaria seleccione una cantidad a apostar, y en dependencia si gana o pierde
 esta cantidad se actualice.
+
 El juego finaliza cuando la usuaria llegue a 200 euros, agrega el código necesario para que se
 muestre un botón Reiniciar Juego que vuelva a iniciar el juego cuando se cumpla esta condición y
 desaparezca el botón de Jugar.
+
 Cuando le demos click a al botón Reiniciar Juego desaparece este botón, poner todos los
 contadores a cero y vuelve a aparecer el botón Jugar.
 Cuando finalice el juego, muestra quién ha ganado la partida: la computadora o la usuaria.
+
 Saca tu lado creativo y maqueta y decora tu página con estilo*/
 
 'use strict';
@@ -22,20 +25,39 @@ function getRandomNumber(max) {
 }
 
 
-const numAle= getRandomNumber(6);
-const number= selectNumber.value ;
+
+const saldo50 = document.querySelector('.class50');
+const saldo100 = document.querySelector('.class100');
 //acertar números
 
 function getMatch() {
     event.preventDefault();
+    const numAle= getRandomNumber(6);
+    const number = parseInt(selectNumber.value);
     console.log(numAle);
     if (number === numAle) {
-        win.classList.remove('collapsed')
+        lose.classList.add('collapsed');
+        win.classList.remove('collapsed');
+        
+
     } else {
-        win.classList.add('.collapsed');
+        win.classList.add('collapsed');
         lose.classList.remove('collapsed');
     }
-}
+};
 
 
 btn.addEventListener('click', getMatch);
+
+
+/* si gana cambia a 100 si pierde a 50 */
+
+/*function saldo() {
+    if (number === numAle) {
+        saldo50.classList.add('collapsed');
+        saldo100.classList.remove('collapsed');
+    }
+}*/
+
+
+
